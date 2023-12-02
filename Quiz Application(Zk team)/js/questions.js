545173f61c677970314b72333ee2837e14b67380
ZahidKhan28 <150408078+ZahidKhan28@users.noreply.github.com>
2023-12-02 21:07:42 +0530

    Add files via upload

---
+++ Quiz Application(Zk team)/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "If a circle has a radius of 5cm, what is its circumference?",
+  answer: "2π*5cm",
+  options: [
+    "4π*5cm",
+    "3π*5cm",
+    "2π*5cm",
+    "1π*5cm"
+  ]
+},
+  {
+  numb: 2,
+  question: "A car travels 120 miles in 2 hours. What is its average speed in miles per hour?",
+  answer: "60 miles per hour",
+  options: [
+    "20 miles per hour",
+    "40 miles per hour",
+    "70 miles per hour",
+    "60 miles per hour"
+  ]
+},
+  {
+  numb: 3,
+  question: "What is 20% 0f 150?",
+  answer: "30",
+  options: [
+    "20",
+    "30",
+    "35",
+    "40"
+  ]
+},
+  {
+  numb: 4,
+  question: "What does SQL stand for?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+  {
+  numb: 5,
+  question: "If a clock shows 3:15, what is the angle between the hour and minute hands?",
+  answer: "20.5",
+  options: [
+    "20",
+    "20.5",
+    "30",
+    "30.5"
+  ]
+},
+
+   {
+   numb: 6,
+   question: "Calculate the simple interest on $5000 at a rate of 8% per annum for 3 years",
+   answer: "$1200",
+   options: [
+     "$1000",
+     "$1200",
+     "$1400",
+     "$1600"
+   ]
+ },
+
+
+
+   {
+   numb: 7,
+   question: "If 6 workers can complete a project in 8 days, how many days will 12 workers take to complete the same project?",
+   answer: "4 days",
+   options: [
+     "4 days",
+     "2 days",
+     "3 days",
+     "8 days"
+   ]
+ },
+
+
+
+   {
+   numb: 8,
+   question: "If you draw a card from a standard deck of 52 cards, what is the probability of getting a heart?",
+   answer: "1/4",
+   options: [
+     "2/4",
+     "4/4",
+     "1/4",
+     "3/4"
+   ]
+ },
+
+
+
+   {
+   numb: 9,
+   question: "A student scored 75 out of 100 in the first exam and 80 out of 100 in the second exam. What is the average percentage score?",
+   answer: "77.5%",
+   options: [
+     "65.5%",
+     "70%",
+     "69.9%",
+     "77.5%"
+   ]
+ },
+
+
+
+
+   {
+   numb: 10,
+   question: "3, 6, 12, 24, ___. What comes next?",
+   answer: "48",
+   options: [
+     "32",
+     "48",
+     "56",
+     "68"
+   ]
+ },
+];
